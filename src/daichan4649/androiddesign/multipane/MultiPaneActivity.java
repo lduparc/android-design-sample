56764cdf6ffc509c7dc38bef1430b4007e40e660
daichan4649 <daichan4649@gmail.com>
2012-10-05 17:53:29 +0900

    Android Design + navigation + multi-pane layouts

---
+++ src/daichan4649/androiddesign/multipane/MultiPaneActivity.java
@@ -0,0 +1,7 @@
+package daichan4649.androiddesign.multipane;
+
+import android.app.Activity;
+
+public class MultiPaneActivity extends Activity {
+
+}
---
+++ src/daichan4649/androiddesign/navigation/NavigationActivity.java
@@ -0,0 +1,7 @@
+package daichan4649.androiddesign.navigation;
+
+import android.app.Activity;
+
+public class NavigationActivity extends Activity {
+
+}
